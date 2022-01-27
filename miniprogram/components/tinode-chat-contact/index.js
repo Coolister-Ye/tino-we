722049bdec4d85edcee1f375fb1f9d6958223303
01379863 <jiataoye@sf-express.com>
2022-01-27 18:05:11 +0800

    update ui

---
+++ miniprogram/components/tinode-chat-contact/index.js
@@ -0,0 +1,46 @@
+// components/tinode-chat-contact/index.js
+Component({
+    /**
+     * Component properties
+     */
+    properties: {
+        avatarPath: {
+            type: String,
+            value: 'user-o'
+        },
+        avatarSize: {
+            type: String,
+            value: '35px'
+        },
+        title: {
+            type: String,
+            value: 'UnKnow'
+        },
+        preview: {
+            type: String,
+            value: 'asasasads1231231223123123123123123123123123123123'
+        },
+        updateTime: {
+            type: String,
+            value: '2021-10-20'
+        },
+        Onlinestatus: {
+            type: String,
+            value: 'online'
+        }
+    },
+
+    /**
+     * Component initial data
+     */
+    data: {
+
+    },
+
+    /**
+     * Component methods
+     */
+    methods: {
+
+    }
+})
